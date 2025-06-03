b7bd688498620b67303bc685682a5c93401c6445
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-06-03 18:29:23 -0300

    EacTemplates: setup with EacRubyUtils::RootModuleSetup.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
+require 'eac_templates'
+require 'eac_ruby_gem_support'
 EacRubyUtils::Rspec.default_setup_create(File.expand_path('..', __dir__))
 EacRubyUtils.require_sub(__FILE__)
